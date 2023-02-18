de150ee759dbec765d6342ad23efbc3a3d56ce08
kamrul25 <kamrul.coder01@gmail.com>
2023-02-18 18:07:09 +0600

    debugging

--- js/main.js
+++ js/main.js
@@ -6,17 +6,24 @@ document.getElementById('triangle-btn').addEventListener('click', function(){
 });
 
 function getElementValueById(firstElement, secondElement, thirdElement){
-    const firstValue = document.getElementById(firstElement).value ;
-    const secondValue = document.getElementById(secondElement).value ;
-    const areaValue = 0.5 * parseFloat(firstValue) * parseFloat(secondValue);
-    const name = document.getElementById(thirdElement).innerText;
-    displayData(name, areaValue);
+    const firstValueString = document.getElementById(firstElement).value ;
+    const secondValueString = document.getElementById(secondElement).value ;
+    const firstValue = parseFloat(firstValueString); 
+    const secondValue = parseFloat( secondValueString); 
+  
+    if(firstValue<0||  secondValue < 0){
+        return alert('Please give a positive number')
+    }else{
+        const areaValue = (0.5 * firstValue * secondValue).toFixed(2);
+        const name = document.getElementById(thirdElement).innerText;
+        displayData(name, areaValue);
+    }
 }
 
 document.getElementById('rectangle-btn').addEventListener('click', function(){
     const firstValue = document.getElementById('rectangle-first-field').value ;
     const secondValue = document.getElementById('rectangle-second-field').value ;
-    const areaValue =  parseFloat(firstValue) * parseFloat(secondValue);
+    const areaValue = ( parseFloat(firstValue) * parseFloat(secondValue)).toFixed(2);
     const name = document.getElementById('rectangle-name').innerText;
     displayData(name, areaValue);
 })
@@ -26,12 +33,26 @@ function displayData(name , areaValue){
     const container = document.getElementById("table-container");
     const tr = document.createElement("tr");
     tr.innerHTML = `
-      <td class="text-black">${serial}.</td>
-      <td class="text-black">${name}</td>
-      <td class="text-black">${areaValue} cm<sup class="text-black">2</sup></td>
+      <td class="text-black">${serial}.${name}</td>
+      <td class="text-black">${areaValue}cm<sup class="text-black">2</sup></td>
       <td>
-      <button class=" bg-blue-500 text-white py-2 font-bold border-none rounded-lg">Convert to m <sup class="text-white">2</sup></button>
+      <button id="convert" class=" bg-blue-500 text-white py-2 font-bold border-none rounded-lg">Convert to m <sup class="text-white">2</sup></button>
       </td>
-    `;
+    `; 
     container.appendChild(tr);
+    // document.getElementById('convert').addEventListener('click', function(e){
+    //     const container = document.getElementById("table-container");
+    //     const tr = document.createElement("tr");
+    //     tr.innerHTML = `
+    //       <td class="text-black">${serial}.${name}</td>
+    //       <td class="text-black">${areaValue/100}m<sup class="text-black">2</sup></td>
+    //       <td><button id="cancel-btn" class=" bg-blue-500 text-white py-2 px-4 font-bold border-none rounded-lg">Cancel</button>
+    //      </td>
+    //     `;
+    //     container.appendChild(tr);
+    //     // document.getElementById('cancel-btn').addEventListener('click', function(e){
+    //     //     e.target.parentNode.parentNode.style.display = 'none';
+    //     // });
+    // });
+  
 }
