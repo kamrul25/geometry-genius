597501ea2be6d1bc455a37dcfc86aeae47b9cbef
kamrul25 <kamrul.coder01@gmail.com>
2023-02-18 21:30:29 +0600

    final commit

---
+++ js/random-color.js
@@ -0,0 +1,8 @@
+// make  random color
+function randomColor() {
+    let color = [];
+    for (let i = 0; i < 3; i++) {
+      color.push(Math.floor(Math.random() * 256));
+    }
+    return 'rgb(' + color.join(', ') + ')';
+  } 
